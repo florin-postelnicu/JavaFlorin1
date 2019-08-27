c2e2347c1f85a5abe912055082896a71f39704fe
florin-postelnicu <30669906+florin-postelnicu@users.noreply.github.com>
2019-08-27 14:42:15 -0700

    Add files via upload

--- Chapter 1 Java/GUI4Pear.java
+++ Chapter 1 Java/GUI4Pear.java
@@ -8,11 +8,12 @@ import java.util.Scanner;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 
- class GUI4Pear {
+ class GUI4Pear implements ActionListener {
 
      String bearing;
 
      G_OOPear3 gamer = new G_OOPear3();
+
      GUI4Pear() {
          JFrame frame = new JFrame("Eddie's  Guessing Game ");
 
@@ -20,7 +21,8 @@ import javax.swing.JFrame;
          JButton play_g = new JButton("Keep_Playing");
          JButton decide_g = new JButton("Choose l-h-e");
          frame.add(start_g, BorderLayout.WEST);
-         frame.add(play_g,BorderLayout.CENTER);
+         start_g.addActionListener((ButtonStart) new ButtonStart());
+         frame.add(play_g, BorderLayout.CENTER);
          frame.add(decide_g, BorderLayout.EAST);
          frame.setLayout(new FlowLayout());
          frame.setSize(600, 300);
@@ -29,34 +31,32 @@ import javax.swing.JFrame;
      }
 
 
+     class ButtonStart extends JButton {
 
-    class ButtonStart extends JButton {
-        JButton button_start = new JButton("Start_Game");
-        public void actionPerformed (ActionListener event) {
-            gamer.Start_game();
+         public  void actionPerformed(ActionListener event) {
+             gamer.Start_game();
 
-        }
-    }
+         }
+     }
 
-    class ButtonKeepPlaying extends JButton{
-        JButton button_keep_playing = new JButton("Keep_Playing");
+     class ButtonKeepPlaying extends JButton {
+         JButton button_keep_playing = new JButton("Keep_Playing");
 
-        public void actionPerformed(ActionListener event){
-            gamer.Keep_Playing();
+         public void actionPerformed(ActionListener event) {
+             gamer.Keep_Playing();
 
-        }
+         }
 
-    }
+     }
 
-    class ButtonDecide extends JButton {
-        public void actionPerformed (ActionListener event){
-            JButton button_decide = new JButton("Choose :l-h-e");
-            gamer.Your_guess_is(bearing);
+     class ButtonDecide extends JButton {
+         public void actionPerformed(ActionListener event) {
+             JButton button_decide = new JButton("Choose :l-h-e");
+             gamer.Your_guess_is(bearing);
 
-        }
-    }
-
-}
+         }
+     }
+ }
 class Test3 {
     public static void main(String[] args) {
         GUI4Pear gamelon = new GUI4Pear();
