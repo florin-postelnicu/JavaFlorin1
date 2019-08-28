71b3e0138da900b54cd67874d9beb92816980169
florin-postelnicu <30669906+florin-postelnicu@users.noreply.github.com>
2019-08-28 14:45:02 -0700

    Add files via upload

--- Chapter 1 Java/GUI4Pear.java
+++ Chapter 1 Java/GUI4Pear.java
@@ -16,7 +16,16 @@ class GUI4Pear  implements ActionListener {
      G_OOPear3 gamer = new G_OOPear3();
 
      GUI4Pear() {
+         JLabel label1 = new JLabel();
+         label1.setText("Enter y/n");
+         label1.setBounds(10,10,200,30);
+
          JFrame frame = new JFrame("Eddie's  Guessing Game ");
+         frame.getContentPane().setLayout(new FlowLayout());
+         JTextField textfield1 = new JTextField("", 100);
+
+
+         textfield1.setBounds(50,250,100,100);
 
          JButton start_g = new JButton("Start_game");
          JButton play_g = new JButton("Keep_Playing");
@@ -27,8 +36,17 @@ class GUI4Pear  implements ActionListener {
          play_g.addActionListener((event -> gamer.Keep_Playing()));
          frame.add(decide_g, BorderLayout.EAST);
          decide_g.addActionListener(event -> gamer.Your_guess_is(bearing));
+         JButton quit_g = new JButton("Quit");
+         frame.add(quit_g, BorderLayout.AFTER_LAST_LINE);
+
+         quit_g.addActionListener(event -> System.exit(0));
+
+
          frame.setLayout(new FlowLayout());
+         frame.add(label1);
+         frame.add(textfield1, BorderLayout.SOUTH);
          frame.setSize(600, 300);
+         frame.pack();
          frame.setVisible(true);
          frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
