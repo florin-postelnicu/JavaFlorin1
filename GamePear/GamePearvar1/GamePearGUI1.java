a1a36c22ea95b4624e8c0f41894a57fc712b2a15
florin-postelnicu <30669906+florin-postelnicu@users.noreply.github.com>
2019-09-02 18:21:57 -0700

    Add files via upload

--- GamePear/GamePearvar1/GamePearGUI1.java
+++ GamePear/GamePearvar1/GamePearGUI1.java
@@ -4,7 +4,7 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 import java.io.PrintStream;
-;
+
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.text.BadLocationException;
@@ -16,41 +16,55 @@ class GamePearGUI  implements ActionListener {
 
     GamePearGUI() {
         JFrame frame = new JFrame("Eddie's  Guessing Game ");
-        frame.getContentPane().setLayout(new GridLayout(3, 3));
+        JPanel panel = new JPanel();
+        frame.getContentPane().setLayout(new GridLayout(3,3));
 
 
         JLabel label1 = new JLabel();
         label1.setText("COMMUNIQUE");
-        label1.setBounds(10,10,10,10);
+//        label1.setBounds(10,10,10,10);
 
 
         JButton yes_g = new JButton("PLAY");
+//        yes_g.setBounds(10,10,20,10);
         JButton no_g = new JButton("CLEAR ");
+//        no_g.setBounds(31,10,20,10);
         JButton less_g = new JButton("LOWER");
+//        less_g.setBounds(52,10,20,10);
         JButton greater_g = new JButton("GREATER");
+//        greater_g.setBounds(52, 21,20,10);
         JButton equal_g = new JButton("EQUAL");
+//        equal_g.setBounds(52, 32, 20, 10);
+        JButton quit_g = new JButton("Quit");
+//        quit_g.setBounds(10,21,60,10);
 
-        JTextArea textArea = new JTextArea(50,50);
+        JTextArea textArea = new JTextArea(80,60);
+        textArea.setLineWrap(true);
+        JScrollPane scroller = new JScrollPane(textArea);
+        scroller.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+        scroller.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_AS_NEEDED);
+        panel.add(scroller);
+
+
         PrintStream standardOut = System.out;
 
         frame.add(yes_g);
         yes_g.addActionListener(event -> game_pear.Start_game());
 
         frame.add(no_g);
-//        no_g.addActionListener(event -> game_pear.Stop_Playing());
-        no_g.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent evt) {
-                // clears the text area
-                try {
-                    textArea.getDocument().remove(0,
-                            textArea.getDocument().getLength());
-                    standardOut.println("Text area cleared");
-                } catch (BadLocationException ex) {
-                    ex.printStackTrace();
-                }
+
+        no_g.addActionListener(evt -> {
+            // clears the text area
+            try {
+                textArea.getDocument().remove(0,
+                        textArea.getDocument().getLength());
+                standardOut.println("Text area cleared");
+                game_pear.Start_game();
+
+            } catch (BadLocationException ex) {
+                ex.printStackTrace();
             }
-        });
+        } );
 
         frame.add(less_g);
         less_g.addActionListener(event -> game_pear.choose_less());
@@ -58,28 +72,22 @@ class GamePearGUI  implements ActionListener {
         frame.add(greater_g);
         greater_g.addActionListener(event -> game_pear.choose_greater());
 
-        frame.add(equal_g);
+         frame.add(equal_g);
         equal_g.addActionListener(event ->game_pear.choose_equal());
 
-        JButton quit_g = new JButton("Quit");
+
         frame.add(quit_g);
 
         quit_g.addActionListener(event -> System.exit(0));
 
         PrintStream printStream = new PrintStream(new CustomOutputStream(textArea));
 
-        textArea.setLayout(new GridBagLayout());
-        GridBagConstraints constraints = new GridBagConstraints();
-        constraints.gridx = 0;
-        constraints.gridy = 0;
-        constraints.insets = new Insets(10, 10, 10, 10);
-        constraints.anchor = GridBagConstraints.WEST;
-
-        frame.add(new JScrollPane(textArea), constraints);
         System.setOut(printStream);
         System.setErr(printStream);
 
-        frame.add(textArea);
+        frame.getContentPane();
+        frame.add(panel);
+        frame.add(label1);
         frame.setSize(600, 300);
         frame.pack();
         frame.setVisible(true);
@@ -91,19 +99,7 @@ class GamePearGUI  implements ActionListener {
         throw new UnsupportedOperationException("Not supported yet");
     }
 
-
-
     }
-
-
-
-
-
-
-
-
-
-
 
 class Test4 {
     public static void main(String[] args) {
