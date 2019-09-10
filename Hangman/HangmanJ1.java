b5859917dd62ce1fab166005605be44aaa7fbb56
florin-postelnicu <30669906+florin-postelnicu@users.noreply.github.com>
2019-09-10 14:43:17 -0700

    Add files via upload

--- Hangman/HangmanJ1.java
+++ Hangman/HangmanJ1.java
@@ -5,6 +5,7 @@ import java.lang.*;
 public class HangmanJ1 {
 
 
+
     public static void main(String[] args) {
 
         Scanner scan = new Scanner(System.in);
@@ -26,36 +27,58 @@ public class HangmanJ1 {
         System.out.println(str);
 
         System.out.println(new_word);
+        System.out.println(new_word.toString());
 
-        boolean isExists = new_word.toString().indexOf("*")!= 0;
 
-        while (isExists){
+        System.out.println(new_word.toString().indexOf("*"));
+        boolean isExists = true;
+        int count = 0;
+
+
+
+        while (isExists) {
+//            boolean itExists;
+
+
 
             // Enter a character
             System.out.println("Enter a new letter : ");
 
             char guess = scan.next().charAt(0);
 
-            char[] chars1 = str.toCharArray();
+
+
 
 
             for (int i = 0; i <= chars.length - 1; i++) {
 
-                char letter = chars1[i];
+//                char letter = chars1[i];
 
-                if (guess == letter) {
+                if (guess == str.charAt(i)) {
                     new_word[i] = guess;
 
-                } else {
-                    continue;
                 }
+                if('*' == new_word [i]){
+                    isExists = true;
+                    continue;
+                }else{
+                    isExists = false;}
+//
+
             }
 
 
             System.out.println(new_word);
 
-
-//
         }
+
+        System.exit(0);
+
+
+
     }
+
+
 }
+
+
