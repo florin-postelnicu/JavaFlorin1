7a6d96670f85bee6f3eeab5f40f9809a7d383dfd
florin-postelnicu <30669906+florin-postelnicu@users.noreply.github.com>
2019-09-10 23:03:52 -0700

    Update HangmanJ2.java

--- Hangman/HangmanJ2.java
+++ Hangman/HangmanJ2.java
@@ -3,10 +3,8 @@
 import java.util.Arrays;
 import java.util.Scanner;
 import java.lang.*;
+
 public class HangmanJ2 {
-
-
-
     public static void main(String[] args) {
 
         Scanner scan = new Scanner(System.in);
@@ -16,53 +14,32 @@ public class HangmanJ2 {
 
         char[] chars = str.toCharArray();
 
-
         for (int i = 0; i <= chars.length - 1; i++) {
 
             chars[i] = '*';
         }
 
-
         System.out.println(str);
-
         System.out.println(chars);
-//        System.out.println(Arrays.toString(chars));
-//
-//
-//        System.out.println(Arrays.toString(chars).indexOf("*"));
         boolean isExists = true;
         int count = 0;
 
-
-
-
-
         while (isExists) {
-//            boolean itExists;
+
             boolean bull = false;
-
-
 
             // Enter a character
             System.out.println("Enter a new letter : ");
 
             char guess = scan.next().charAt(0);
 
-
-
-
-
-
             for (int i = 0; i <= chars.length - 1; i++) {
-
-//                char letter = chars1[i];
 
                 if (guess == str.charAt(i)) {
                     chars[i] = guess;
                     bull = true;
                 }
             }
-
             if(!Arrays.toString(chars).contains("*")){
                 System.out.println("This is the word! Congrats!");
                 System.exit(0);
@@ -72,23 +49,10 @@ public class HangmanJ2 {
                 if(count >= 7){
                     System.out.println("Bad !  Bad1   Bad!");
                    System.exit(0);
-
                 }
             }
-//            isExists = bull;
-
-
-
             System.out.println(chars);
-
         }
-
         System.exit(0);
-
-
-
     }
-
-
 }
-
