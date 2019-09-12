aff81a64b48fd4d3d1826828a5f70e59416c6c53
florin-postelnicu <30669906+florin-postelnicu@users.noreply.github.com>
2019-09-12 06:37:46 -0700

    Add files via upload

---
+++ Hangman/HangmanJ3.java
@@ -0,0 +1,86 @@
+
+
+import java.util.Arrays;
+import java.util.Scanner;
+import java.lang.*;
+public class HangmanJ3 {
+    boolean repeat_in_ML;
+
+
+
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+         String str = "hangmanGame";
+         char[] chars = str.toCharArray();
+
+
+        // String to characters array
+
+
+        char[] memoryLane = new char[chars.length + 7];
+        int index_memory = 0;
+
+
+        for (int i = 0; i <= chars.length - 1; i++) {
+
+            chars[i] = '*';
+        }
+
+
+        System.out.println(str);
+
+        System.out.println(chars);
+        int count = 0;
+
+        while (true) {
+
+            boolean bull = false;
+
+            // Enter a character
+            System.out.println("Enter a new letter : ");
+
+            char guess = scan.next().charAt(0);
+
+            for (int i = 0; i <= chars.length - 1; i++) {
+
+                if (guess == str.charAt(i)) {
+                    chars[i] = guess;
+                    bull = true;
+
+                }
+            }
+
+            if(!Arrays.toString(chars).contains("*")){
+                System.out.println("This is the word! Congrats!");
+                System.exit(0);
+            }
+            if (!bull | toString(guess, memoryLane) ) {
+
+                count++;
+                System.out.println("You made Soooooo many mistakes   :" + count);
+                if(count >= 7){
+                    System.out.println("Bad !  Bad1   Bad!");
+                   System.exit(0);
+                }
+            }
+            memoryLane[index_memory] = guess;
+            System.out.println(memoryLane);
+            index_memory++;
+            System.out.println(chars);
+        }
+    }
+
+    private static boolean toString(char guesst, char[] aRRay) {
+        for(int i = 0; i <= 7; i++ ){
+            if ( aRRay[i] == guesst ){
+                return true;
+
+            }
+        }
+
+
+        return false;
+    }
+}
+
