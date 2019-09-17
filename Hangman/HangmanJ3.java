46b52a3107c288811c4c9195d2995e3d05e65807
florin-postelnicu <30669906+florin-postelnicu@users.noreply.github.com>
2019-09-17 11:18:05 -0700

    Add files via upload

--- Hangman/HangmanJ3.java
+++ Hangman/HangmanJ3.java
@@ -7,118 +7,126 @@ import java.util.Scanner;
 import java.lang.*;
  class HangmanJ3 {
 
-    private String word;
 
-    Scanner scan = new Scanner(System.in);
+     private String word;
 
-     HangmanJ3(){
+     Scanner scan = new Scanner(System.in);
+
+     HangmanJ3() {
          SelectWord neword = new SelectWord();
-        word = neword.getWord() ;
-    }
-    private  void  setWord( String w) {
-       this.word = w;
-    }
-
-    private  String getWordo (){
-        return word;}
+         word = neword.get_a_new_Word();}
 
 
-    public void  Hange() {
-        HangmanJ3 hang = new HangmanJ3();
+         private void setWord (String w){
+             SelectWord neword = new SelectWord();
+             word = neword.get_a_new_Word();
+             this.word = w;
+         }
+
+         private String getWord () {
+             return word;
+         }
+
+
+         public void Hange () {
+             HangmanJ3 hang = new HangmanJ3();
 //
 //        Scanner scan = new Scanner(System.in);
-        String str = hang.getWordo();
-        char[] chars = str.toCharArray();
+             String str = hang.getWord();
+             char[] chars = str.toCharArray();
 
 
-        // String to characters array
+             // String to characters array
 
 
-        char[] memoryLane = new char[chars.length + 8];
-        int index_memory = 0;
+             char[] memoryLane = new char[chars.length + 8];
+             int index_memory = 0;
 
 
-        for (int i = 0; i <= chars.length - 1; i++) {
+             for (int i = 0; i <= chars.length - 1; i++) {
 
-            chars[i] = '*';
-        }
-        System.out.println(str);
+                 chars[i] = '*';
+             }
+             System.out.println(str);
 
-        System.out.println(chars);
-        int count = 0;
+             System.out.println(chars);
+             int count = 0;
 
-        while (true) {
+             while (true) {
 
-            boolean bull = false;
+                 boolean bull = false;
 
-            // Enter a character
-            System.out.println("Enter a new letter : ");
+                 // Enter a character
+                 System.out.println("Enter a new letter : ");
 
-            char guess = scan.next().charAt(0);
+                 char guess = scan.next().charAt(0);
 
 //            char guess = PlayHG();
 
-            for (int i = 0; i <= chars.length - 1; i++) {
+                 for (int i = 0; i <= chars.length - 1; i++) {
 
-                if (guess == str.charAt(i)) {
-                    chars[i] = guess;
-                    bull = true;
+                     if (guess == str.charAt(i)) {
+                         chars[i] = guess;
+                         bull = true;
 
-                }
-            }
+                     }
+                 }
 
-            if(!Arrays.toString(chars).contains("*")){
-                System.out.println("This is the word!" );
-                System.out.println(chars );
-                System.out.println("Congrats!");
-                System.exit(0);
-            }
-            if (!bull | Memory_Repeat(guess, memoryLane) ) {
+                 if (!Arrays.toString(chars).contains("*")) {
+                     System.out.println("This is the word!");
+                     System.out.println(chars);
+                     System.out.println("Congrats!");
+                     System.exit(0);
 
-                count++;
+                 }
+                 if (!bull | Memory_Repeat(guess, memoryLane)) {
 
-                if(count >= 7){
-                    System.out.println("Bad !  Bad!   Bad!");
-                    System.exit(0);
-                }
-            }
-            memoryLane[index_memory] = guess;
+                     count++;
 
-            System.out.println("You made Soooooo many mistakes   :" + count);
+                     if (count >= 7) {
+                         System.out.println("Bad !  Bad!   Bad!");
+                         System.exit(0);
+                     }
+                 }
+                 memoryLane[index_memory] = guess;
+                 index_memory++;
 
-            System.out.println("Your memory's bellow !");
+                 System.out.println("You made Soooooo many mistakes   :" + count);
 
-            System.out.println( memoryLane);
-            index_memory++;
-            System.out.println("That's the status of your work on the secret word :");
-            System.out.println(chars);
-        }
-    }
+                 System.out.println("Your memory's bellow !");
 
-    private static boolean Memory_Repeat(char guesst, char[] aRRay) {
-        HangmanJ3 hango = new HangmanJ3();
-        for(int i = 0; i <= hango.getWordo().length() + 7; i++ ){
-            if ( aRRay[i] == guesst ){
-                return true;
-            }
-        }
-        return false;
-    }
+                 System.out.println(memoryLane);
 
-    public void DisplayHG(){
-        System.out.println("Let's the game began!");
-    }
-    private char PlayHG(){
-        char g =  scan.next().charAt(0);
+                 System.out.println("That's the status of your work on the secret word :");
+                 System.out.println(chars);
+             }
+         }
 
-        return g;
+         private static boolean Memory_Repeat ( char guesst, char[] aRRay){
+             HangmanJ3 hango = new HangmanJ3();
+             for (int i = 0; i <= hango.getWord().length() + 6; i++) {
+                 if (aRRay[i] == guesst) {
+                     return true;
+                 }
+             }
+             return false;
+         }
 
-    }
-}
+         public void DisplayHG () {
+             System.out.println("Let's the game began!");
+         }
+
+         private char PlayHG () {
+             char g = scan.next().charAt(0);
+
+             return g;
+         }
+
+     }
+
 
 class TestHangaman {
     public static void main(String[] args) {
-
 
     HangmanJ3 hangman_game = new HangmanJ3();
     hangman_game.Hange();
