278b520a56ad847526888234a6e159dcad4a9180
florin-postelnicu <30669906+florin-postelnicu@users.noreply.github.com>
2019-09-17 20:06:29 -0700

    Add files via upload

---
+++ Hangman/HangmanJ6.java
@@ -0,0 +1,38 @@
+/*
+
+ */
+import java.util.Arrays;
+import java.util.Scanner;
+import java.lang.*;
+
+class HangmanJ6 {
+    Scanner scan = new Scanner(System.in);
+
+    public char Invite_play() {
+        System.out.println("Do you want to play one more game y\\n");
+
+        char invite = scan.next().charAt(0);
+        return invite;
+    }
+    public HangmanJ6() {
+        if(Invite_play()!='y'){
+            System.exit(0);
+
+        }
+        else{
+            HangmanJ4 hangman_game = new HangmanJ4();
+            hangman_game.Hange();
+
+        }
+    }
+
+
+}
+
+
+class TestH5{
+    public static void main(String[] args) {
+        HangmanJ6 play = new HangmanJ6();
+
+    }}
+
