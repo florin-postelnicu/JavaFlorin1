6afa03142b7db80cd764b287377b29c7d0566fad
florin-postelnicu <30669906+florin-postelnicu@users.noreply.github.com>
2019-09-19 13:24:16 -0700

    Add files via upload

---
+++ Hangman/HangmanUltimatum/HangmanJ6.java
@@ -0,0 +1,31 @@
+/*hangmanJ4 and SelectWord are the main ingredients for HangmanJ6-the play machine
+ */
+
+import java.util.Scanner;
+import java.lang.*;
+
+class HangmanJ6 {
+    Scanner scan = new Scanner(System.in);
+
+    public char Invite_play() {
+        System.out.println("Do you want to play one more game y\\n");
+
+        char invite = scan.next().charAt(0);
+        return invite;
+    }
+    public HangmanJ6() {
+        if(Invite_play()!='y'){
+            System.out.println("Thank you for playing HangmanJ  !");
+            System.exit(0);
+        }
+        else{
+            HangmanJ4 hangman_game = new HangmanJ4();
+            hangman_game.Hange();
+        }
+    }
+}
+
+class PlayDaGame{
+    public static void main(String[] args) {
+        new HangmanJ6();
+    }}
