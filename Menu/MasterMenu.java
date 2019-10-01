d4985f815d260ec7f4a6c33fec2ef3e0b4287fe6
florin-postelnicu <30669906+florin-postelnicu@users.noreply.github.com>
2019-10-01 11:58:19 -0700

    Add files via upload

--- Menu/MasterMenu.java
+++ Menu/MasterMenu.java
@@ -22,10 +22,29 @@ public class MasterMenu {
 
                     break;
                 case 2:
+                    System.out.println("Sorry! Companions - We are under construction! Use an alter ego!");
+                    break;
+
+                case 3:
+                    System.out.println("Sorry! Curatives - We are under construction!Use some mint!");
+                    break;
+                case 4:
+                    System.out.println("Sorry! Magic -  We are under construction! Use some scary words");
+                    break;
+                case 5:
+                    System.out.println("Sorry! Maps-  We are under construction! Use your intuition");
+                    break;
+                case 6:
+                    System.out.println("Sorry! Armor- We are under construction!\n Now we have available only smoke screen!");
+                    break;
+
+
+
+                case 7:
                     System.out.println("Game Over!");
                     System.exit(0);
                     break;
-                case 3:
+                case 8 :
                     System.out.println("Maybe other time y/ n if you want to play more");
                     char kai = scan.next().charAt(0);
                     if (kai != 'y'){
@@ -34,6 +53,7 @@ public class MasterMenu {
                     MasterMenu.display_things();
 
                     break;
+
 
                 default:
                     System.out.println("You should make a valid choice 1-2-3");
@@ -47,8 +67,12 @@ public class MasterMenu {
     public static void display_things(){
         System.out.println("This is the Master Menu");
         System.out.println("Press 1 for Weapons");
-        System.out.println("Press 2 for Quit");
-        System.out.println("Press 3 for something");
+        System.out.println("Press 2 for Companions ");
+        System.out.println("Press 3 for Curatives");
+        System.out.println("Press 4 for Magic");
+        System.out.println("Press 5 for Maps");
+        System.out.println("Press 6 for Armor");
+        System.out.println("Press 7 for Quit ");
     }
 
 }
