e2dfb018f741bdc21e4addfa1e066b4c3fd3c04a
florin-postelnicu <30669906+florin-postelnicu@users.noreply.github.com>
2019-11-11 08:23:02 -0700

    Add files via upload

---
+++ Formatted/Primecl.java
@@ -0,0 +1,25 @@
+import com.sun.org.apache.bcel.internal.generic.ARETURN;
+
+import java.lang.Math;
+public class Primecl {
+    int msg;
+
+    public  int checkprime(int n) {
+        int detect = 0;
+        int up = (int)Math.sqrt( n) + 1;
+        for (int i = 2; i < up; i++) {
+            if (n % i == 0) {
+                detect = 1;
+
+            }
+
+        }
+        return detect;
+    }}
+
+// t{
+//    public static void main(String[] args) {
+//        Primecl primecl = new Primecl();
+//        System.out.println(primecl.checkprime(7));
+//    }
+//}
