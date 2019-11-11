964a620acfe47fd45597e25d45c90a9995bde166
florin-postelnicu <30669906+florin-postelnicu@users.noreply.github.com>
2019-11-11 14:56:03 -0700

    Add files via upload

--- Formatted/FinddamPrimes.java
+++ Formatted/FinddamPrimes.java
@@ -13,10 +13,8 @@ public class FinddamPrimes {
 
     public static void main(String[] args) {
         ArrayList array1 = new ArrayList<Integer>();
-//        Formatter fmt = new Formatter();
 
-
-        for (int i = 2; i <= 100000; i++) {
+        for (int i = 2; i <= 1000000; i++) {
             Primecl primecl = new Primecl();
 
             if (primecl.checkprime(i) == 0) {
@@ -37,19 +35,22 @@ public class FinddamPrimes {
         }catch( IndexOutOfBoundsException e){
             System.out.println("");
         }
+        int k = 0;
+         for(Iterator iterator = array1.iterator();iterator.hasNext();){
+             k = k+1;
+             if(k%10 == 0){
+                 System.out.println("\n");
+             }
+             else{
+
+             System.out.printf("%10d", iterator.next());
+         }
+
+        }
 
 
-//        for (Iterator<Integer> iter= array1.iterator();iter.hasNext();) {
-//
-//            fmt.format("%10s", iter.next());
-//
-//    }
-//
-//
-//        System.out.print(fmt);
+}}
 
-}
-}
 
 
 
