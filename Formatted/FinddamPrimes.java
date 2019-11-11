12f7a420172de757f03214e01a37a74f1cca478e
florin-postelnicu <30669906+florin-postelnicu@users.noreply.github.com>
2019-11-11 08:21:44 -0700

    Add files via upload

--- Formatted/FinddamPrimes.java
+++ Formatted/FinddamPrimes.java
@@ -1,43 +1,62 @@
-import org.w3c.dom.ls.LSOutput;
-import java.util.Iterator;
-import java.util.Formatter;
+
+/*
+Use printf to create the format you need, without invoking java.util.Formatter
+https://www.geeksforgeeks.org/understanding-array-indexoutofbounds-exception-in-java/
+ */
 
 import java.util.ArrayList;
+//import java.util.Formatter;
+import java.util.Iterator;
+import java.lang.IndexOutOfBoundsException;
 
 public class FinddamPrimes {
+
     public static void main(String[] args) {
-        ArrayList array1 = new ArrayList();
-        Formatter fmt = new Formatter();
-        ;
-        for ( int i = 2; i<= 100; i++){
-            CheckPrimes checkPrimes = new CheckPrimes();
-            if(checkPrimes.checdamnums(i) == 0){
+        ArrayList array1 = new ArrayList<Integer>();
+//        Formatter fmt = new Formatter();
+
+
+        for (int i = 2; i <= 100000; i++) {
+            Primecl primecl = new Primecl();
+
+            if (primecl.checkprime(i) == 0) {
                 array1.add(i);
             }
         }
+        System.out.println(array1.size());
+        System.out.println(array1.get(3));
+        try {
+            for (int i = 0; i <= array1.size() / 5; i++) {
+                for (int j = 0; j < 5; j++) {
+                    System.out.printf("%10d", array1.get(i * 5 + j));
+                }
 
-        for (Iterator<Integer> iter= array1.iterator();iter.hasNext();) {
+                System.out.println("\n");
 
-
-
-            fmt.format("%10d", iter.next());
-
-
-
-
-
+            }
+        }catch( IndexOutOfBoundsException e){
+            System.out.println("");
         }
 
 
-        System.out.print(fmt);
+//        for (Iterator<Integer> iter= array1.iterator();iter.hasNext();) {
+//
+//            fmt.format("%10s", iter.next());
+//
+//    }
+//
+//
+//        System.out.print(fmt);
 
-        System.out.println();
+}
+}
 
 
 
 
 
-    }}
+
+
 
 
 
