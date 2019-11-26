dbffbd86ab5df2e3c0014ebf69d9e66a02b66648
florin-postelnicu <30669906+florin-postelnicu@users.noreply.github.com>
2019-11-26 07:48:57 -0700

    Add files via upload

--- ElementaryJ/TestforCars.java
+++ ElementaryJ/TestforCars.java
@@ -1,24 +1,18 @@
 public class TestforCars {
     public static void main(String[] args) {
-        Cars BillyCar = new Cars("Ford", "Mustang", "Red");
-        System.out.println("Billy's car is a : "+ BillyCar.brand +
-                "- "+BillyCar.model +" and its color is :"+BillyCar.color);
-        Cars myCar = new Cars("Toyota","Prius","Silver");
-        System.out.println("My Car's brand is :" + myCar.brand);
-        System.out.println("My Car's model is :" + myCar.model);
-        System.out.println("My Car's color is :" + myCar.color);
 
-        Cars Biak = new Cars("Buagatti", "236ZXL");
-        System.out.println("Biak's brnd is :" + Biak.brand);
-        System.out.println("Biak's model is :" + Biak.model);
-        System.out.println("Biak's color is :" + Biak.color);
-        Biak.setBrand("Fiat");
-        System.out.println(Biak.getBrand());
 
-        Cars rabla = new Cars();
-        System.out.println(rabla.brand);
-        System.out.println(rabla.model);
-        System.out.println(rabla.color);
+        System.out.println("Minivan");
+        Minivan familyCar = new Minivan(7, 5);
+        familyCar.printCarCahracters();
+
+        System.out.println("After setting the attributes");
+        familyCar.setBrand("Ford");
+        familyCar.setModel(" Windstar");
+        familyCar.setColor(" Blue");
+        familyCar.printCarCahracters();
+
+
 
 
     }
