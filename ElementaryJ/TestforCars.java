0fb7831cf70c1aaf9eeffe1a849db7d202cc52f5
florin-postelnicu <30669906+florin-postelnicu@users.noreply.github.com>
2019-12-02 08:10:21 -0700

    Add files via upload

--- ElementaryJ/TestforCars.java
+++ ElementaryJ/TestforCars.java
@@ -4,16 +4,24 @@ public class TestforCars {
 
         System.out.println("Minivan");
         Minivan familyCar = new Minivan(7, 5);
-        familyCar.printCarCahracters();
+        familyCar.printCarAttributes();
 
         System.out.println("After setting the attributes");
         familyCar.setBrand("Ford");
         familyCar.setModel(" Windstar");
         familyCar.setColor(" Blue");
-        familyCar.printCarCahracters();
+        familyCar.filltank(121);
+        familyCar.runtheCar();
+        familyCar.printCarAttributes();
 
-
-
+        Cars tonyC = new Cars();
+        tonyC.printCarAttributes();
+        familyCar.runtheCar();
+        System.out.println(familyCar.getGas());
+        System.out.println(tonyC.gas);
+        System.out.println(tonyC.currency);
+        System.out.println(tonyC.brand);
+        tonyC.runtheCar();
 
     }
 
