3e0b2fe1c97e41a136f2f9cce62dcc2b9272305a
florin-postelnicu <30669906+florin-postelnicu@users.noreply.github.com>
2019-12-02 08:09:49 -0700

    Add files via upload

--- ElementaryJ/Minivan.java
+++ ElementaryJ/Minivan.java
@@ -5,16 +5,32 @@
 public class Minivan extends Cars{
     int seats ;
     int doors;
-    public Minivan(int theSetas, int theDoors){
+    int towing = 2 ;
+
+    public Minivan(int theSeats, int theDoors){
         super();
-        this.seats = theSetas;
+        this.seats = theSeats;
+        this.doors = theDoors;}
+
+    public Minivan(int theSeats, int theDoors,int theTowing){
+        super();
+        this.seats = theSeats;
         this.doors = theDoors;
+        this.towing = theTowing;
     }
+    public void setTowing(int tow){
+        this.towing = tow;
+    }
+//    public double getTowing(){
+//        return  this.towing;
+//    }
+
 
     @Override
-    public void printCarCahracters() {
+    public void printCarAttributes() {
         System.out.println(" Brand : "+ this.getBrand() + "\nModel  :" + this.getModel() + "\nColor : " + this.getColor() +
-                 "\nSeats : " + this.seats + "\nDoors : " + this.doors);
+                 "\nSeats : " + this.seats + "\nDoors : " + this.doors + "\nTowing Capacity :"+ this.towing);
 
     }
+
 }
