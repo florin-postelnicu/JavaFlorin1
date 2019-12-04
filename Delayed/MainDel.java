a62ecee5963ef9be28247ad456857c82f6cb7cbb
florin-postelnicu <30669906+florin-postelnicu@users.noreply.github.com>
2019-12-04 09:08:24 -0700

    Add files via upload

---
+++ Delayed/MainDel.java
@@ -0,0 +1,22 @@
+
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.DelayQueue;
+import java.util.concurrent.Delayed;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
+public class MainDel {
+    public static void main(String[] args) throws InterruptedException, ExecutionException
+    {
+        DelayQueue<DelayedEvent> queue = new DelayQueue<>();
+        AtomicInteger counter = new AtomicInteger();
+        ScheduledExecutorService ses = Executors.newScheduledThreadPool(2);
+        ses.scheduleAtFixedRate(new DelayedEventProducer(queue, counter), 1, 2, TimeUnit.SECONDS);
+        ses.scheduleAtFixedRate(new DelayedEventConsumer(queue), 1, 10, TimeUnit.SECONDS);
+    }
+}
