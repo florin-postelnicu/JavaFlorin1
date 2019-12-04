1d31e6fcd3524b92d186a70e52f06833500fec70
florin-postelnicu <30669906+florin-postelnicu@users.noreply.github.com>
2019-12-04 11:39:11 -0700

    Add files via upload

--- ElementaryJ/TestforCars.java
+++ ElementaryJ/TestforCars.java
@@ -1,28 +1,24 @@
+import java.util.ArrayList;
+
 public class TestforCars {
     public static void main(String[] args) {
 
+        ArrayList<Cars> CarsList = new ArrayList<>();
 
-        System.out.println("Minivan");
-        Minivan familyCar = new Minivan(7, 5);
-        familyCar.printCarAttributes();
+        for (int i = 1; i<=5; i++){
 
-        System.out.println("After setting the attributes");
-        familyCar.setBrand("Ford");
-        familyCar.setModel(" Windstar");
-        familyCar.setColor(" Blue");
-        familyCar.filltank(121);
-        familyCar.runtheCar();
-        familyCar.printCarAttributes();
+           CarsList.add(new Cars("Car" +i));
 
-        Cars tonyC = new Cars();
-        tonyC.printCarAttributes();
-        familyCar.runtheCar();
-        System.out.println(familyCar.getGas());
-        System.out.println(tonyC.gas);
-        System.out.println(tonyC.currency);
-        System.out.println(tonyC.brand);
-        tonyC.runtheCar();
+        }
+        for(Cars car : CarsList){
+            int speed = 400*((int)(5*Math.random()+ 1 ));
+            car.speed = speed;
+            car.gas = 5;
 
-    }
+            car.thread.start();
 
+
+
+        }
+}
 }
