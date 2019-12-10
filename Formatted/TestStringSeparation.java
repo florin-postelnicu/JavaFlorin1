4f1b2c8db13592c00013c5afa9c83887e76f968e
florin-postelnicu <30669906+florin-postelnicu@users.noreply.github.com>
2019-12-10 10:35:11 -0700

    Add files via upload

--- Formatted/TestStringSeparation.java
+++ Formatted/TestStringSeparation.java
@@ -5,21 +5,28 @@ public class TestStringSeparation {
 
     public static void main(String[] args) {
 
+        for( int i = 10101; i<=1010101; i++) {
 
-        ConvertNumberIntoCharArray cvna = new ConvertNumberIntoCharArray();
-        char[] newList = cvna.ZArrayList(203406043);
-        if (cvna.CheckPoly(newList) == 0){
-            System.out.println("POLY");
 
-        }
-        else{
-            System.out.println("Nope");
-        }
-        System.out.println(newList);
-        System.out.println(newList.length);
+            ConvertNumberIntoCharArray cvna = new ConvertNumberIntoCharArray();
+            char[] newList = cvna.ZArrayList(i);
+            int k = 0;
+            if (cvna.CheckPoly(newList) == 0) {
 
-    }
+                for(int j = 0; j<= 10; j++){
+                    k = k+1;
+                    if(k%10 == 0){
+                        System.out.println("\n");
+
+
+                    }else{
+                    System.out.printf("%10d",i);
+                }
+                    
         }
+
+    }}}}
+
 
 
 
