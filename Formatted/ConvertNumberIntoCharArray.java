b69c430587abdd6cc6f5b240238a332533cf7462
florin-postelnicu <30669906+florin-postelnicu@users.noreply.github.com>
2019-12-11 09:09:16 -0700

    Add files via upload

--- Formatted/ConvertNumberIntoCharArray.java
+++ Formatted/ConvertNumberIntoCharArray.java
@@ -4,15 +4,17 @@ public class ConvertNumberIntoCharArray {
 
     int number;
 
-    public char[] ZArrayList(int Znumber){
+    public int ZPalindrome(int Znumber){
         this.number = Znumber;
 
     String st = String.valueOf(this.number);
     char[] list = st.toCharArray();
-    return list;}
+
+    return CheckPoly(list);
+    }
     public int CheckPoly (char[] newList1) {
         int santinel = 0;
-        newList1 = ZArrayList(this.number);
+
         for (int i = 0; i <= newList1.length / 2; i++) {
             if (newList1[i] != newList1[newList1.length - 1 - i]) {
                 santinel = 1;
