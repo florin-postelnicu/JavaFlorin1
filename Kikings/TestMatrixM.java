635703b7b2a0774831ce3ca8e338b5df7b314997
florin-postelnicu <30669906+florin-postelnicu@users.noreply.github.com>
2019-12-24 23:50:46 -0700

    Add files via upload

--- Kikings/TestMatrixM.java
+++ Kikings/TestMatrixM.java
@@ -3,9 +3,19 @@
 import sun.rmi.runtime.NewThreadAction;
 
 import java.io.PrintStream;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+
+import java.util.Arrays;
+import java.util.Scanner;
 import java.util.*;
 
- class MatrixM {
+ class Matrix {
 
 
 
@@ -13,15 +23,51 @@ import java.util.*;
 
     // I am creating a Temp matrix
 
-    public static int[][] MatrixM(){
-        int[][] Matrix = new int[6][7];
-        for(int row = 0; row < 6; row++) {
-            for (int col = 0; col < 7; col++) {
-               Matrix[row][col] = 0;
-        }
-    }
-    return Matrix;
-    }
+     public static  int[][] MatrixM() throws Exception{
+         int[][] myArray;
+         FileReader fr = new FileReader("/home/florin/IdeaProjects/Viking/src/matrix.txt");
+         Scanner sc = new Scanner(new BufferedReader(fr));
+
+
+                 int rows = 6;
+                 int columns = 7;
+                 myArray = new int[rows][columns];
+                 while (sc.hasNextLine()) {
+                     // Split in lines; number of lines = size of array
+                     for (int i = 0; i < myArray.length; i++) {
+                         String[] line = sc.nextLine().trim().split("   ");
+                         // Split each line in entries, by reading each integer
+                         // split by how many blanks are in between
+                         for (int j = 0; j < line.length; j++) {
+                             myArray[i][j] = Integer.parseInt(line[j]);
+                         }
+                     }
+                 }
+
+//             System.out.println(Arrays.deepToString(myArray));
+             return myArray;
+     }
+
+     public void Write2DMatrix(int[][] matrixes) throws Exception{
+         BufferedWriter bw = new BufferedWriter(new FileWriter(new File("/home/florin/IdeaProjects/Viking/src/matrix.txt")));
+
+         StringBuffer sb = new StringBuffer();
+
+         for(int r =0;r< 6;r++){
+             for(int c = 0; c< 7;c++){
+                 sb.append(matrixes[r][c]+"   ");
+             }
+             sb.append("\n");
+         }
+
+         bw.write(sb.toString());
+         bw.close();
+     }
+
+
+
+
+
 
      public static int[][] UpdateMatrix(int[][] Matrix,int r, int c, int val){
          int updatedMatrix [][]= new int[6][7];
@@ -50,28 +96,22 @@ import java.util.*;
              //Generally path.add(new int[]{row, column}) of the element chosen in the path
 
          return path;
-
-
      }
-
-
 
 
      // New Updates for o whole vector
 
          public static int[][]UpdateMatrixVec(int [][] Matrix, List<int[]> vec){
 
-        for( int index = 0; index<  vec.size(); index ++){
             for(int row = 0; row <6; row++){
                 for(int col = 0; col <7; col++){
-                    if(row != vec.get(index)[0] || col != vec.get(index)[1]){
-                        Matrix[row][col] = Matrix[row][col] ;
+             for( int index = 0; index<  vec.size(); index ++){
+                    if(row == vec.get(index)[0] && col == vec.get(index)[1]){
+                        Matrix[row][col] = Matrix[row][col] +1 ;
 
                     }else{
-                        System.out.println(vec.get(index)[0]  + "    "+ vec.get(index)[1]);
-                        System.out.println("Yep");
-                        Matrix[row][col] = Matrix[row][col] +1;
-                        System.out.println("Matrix Entry   " + Matrix[row][col]);
+
+                        Matrix[row][col] = Matrix[row][col] ;
                     }
                     }
                 }
@@ -116,8 +156,11 @@ import java.util.*;
 
 }
 public class TestMatrixM {
-    public static void main(String[] args) {
-        MatrixM matrix = new MatrixM();
+    public static void main(String[] args) throws Exception {
+
+        // The Original Matrix
+        System.out.println("\n Original matrix");
+        Matrix matrix = new Matrix();
 
         for (int r = 0; r < 6; r++) {
             for (int c = 0; c < 7; c++) {
@@ -125,33 +168,10 @@ public class TestMatrixM {
             }
             System.out.println("\n");
         }
-        Vector<Integer> vec = new Vector<>();
-        vec.add(0, matrix.MatrixM()[0][0] + 8);
-        vec.add(1, matrix.MatrixM()[0][1] + 7);
-        vec.add(2, matrix.MatrixM()[0][2] + 1);
-
-        MatrixM.SetEntry1(MatrixM.MatrixM(), 0, 0, vec.get(0));
 
 
-        MatrixM.SetEntry1(MatrixM.MatrixM(), 0, 1, vec.get(1));
-
-
-        MatrixM.SetEntry1(MatrixM.MatrixM(), 0, 2, vec.get(2));
-
-        System.out.println("\n\nNew Matrix");
-
-        for(int r = 0; r< 6; r++) {
-            for(int c = 0 ; c <7; c++){
-
-
-            System.out.printf("%10d",MatrixM.UpdateMatrix(MatrixM.MatrixM(), 0, 0, vec.get(0))[r][c]);
-        }
-            System.out.println("\n");
-        }
-
-        MatrixM.UpdateMatrix(MatrixM.MatrixM(), 0, 0, vec.get(0));
-        System.out.println("\nSecond Iteration");
-        int [][] NewMatrix = MatrixM.UpdateMatrixVec(MatrixM.MatrixM(), MatrixM.Vectors2D());
+        System.out.println("\nUpdated Matrix");
+        int [][] NewMatrix = Matrix.UpdateMatrixVec(Matrix.MatrixM(), Matrix.Vectors2D());
 
 
         for(int r = 0; r< 6; r++) {
@@ -162,6 +182,9 @@ public class TestMatrixM {
             }
             System.out.println("\n");
         }
+        matrix.Write2DMatrix(NewMatrix);
+
 
     }
+
 }
