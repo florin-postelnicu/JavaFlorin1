974c7ec315ac029c07ec00dcefaf4484df744f76
florin-postelnicu <30669906+florin-postelnicu@users.noreply.github.com>
2019-12-30 16:19:45 -0700

    Add files via upload

--- ConnectFourJ/ComputerPlayer.java
+++ ConnectFourJ/ComputerPlayer.java
@@ -1,17 +1,9 @@
-/* ************************************************************************* *\
-*                Programmierung 1 HS 2019 - Serie 5-1                         *
-\* ************************************************************************* */
-
+import java.util.ArrayList;
+import java.util.List;
 
 /** A very stupid computer player */
-public class ComputerPlayer implements IPlayer
-{
+public class ComputerPlayer implements IPlayer {
     private Token token;
-
-
-//Diese Methode ermittelt sowohl die Möglichkeit des Gegners
-//als auch die des Computers auf einen Sieg
-
 
     public boolean checkPossibility(Token[][] board, int col, int row)
     {
@@ -124,17 +116,61 @@ public class ComputerPlayer implements IPlayer
         else
             return false;
     }
+    public static int[][] EddieMemory() throws Exception {
+        int[][] myEddie;
 
 
-    /**
-     * Strategy is to chose a random column and select
-     * the next valid column to the right (the chosen included)
-     */
+        if(VierGewinnt.firstPlayer == 1){
+            myEddie= VierGewinnt.matrix1();
 
-    //Der Computer sollte sowohl einen "offensichtlichen" Sieg des Gegners verhindern
-    // als auch Selbst eine Gewinchance ausnutzen
-    public int getNextColumn( Token[][] board )
-    {
+        }
+        else{
+            myEddie = VierGewinnt.matrix2();
+        }
+
+        return myEddie;
+
+    }
+
+    public static List<int []> VectorChoices(int[][] eddieMemory, List<int []> eddieChoices){
+        List<int []> myVectorChoices = new ArrayList<>();
+        for(int indx = 0; indx < eddieChoices.size(); indx++){
+            int valed = eddieMemory[eddieChoices.get(indx)[0]][eddieChoices.get(indx)[1]];
+            int rowed = eddieChoices.get(indx)[0];
+            int coled = eddieChoices.get(indx)[1];
+            myVectorChoices.add(new int[]{valed, rowed, coled});
+        }
+
+       return myVectorChoices;
+    }
+    public static int MaxOlista1(List<int[]> vec) {
+        int max = 0;
+        int row;
+        int col;
+        int[] array = new int[3];
+
+        for (int[] ints : vec) {
+
+            if (max <= ints[0]) {
+                max = ints[0];
+                array[0] = max;
+                row = ints[1];
+                array[1] = row;
+                col = ints[2];
+                array[2] = col;
+
+            }
+
+        }
+        return array[2];
+    }
+
+
+
+
+
+
+    public Object getNextColumn(Token[][] board ) throws Exception {
         if(board[3][0]== Token.empty){
             return 3;
 
@@ -160,10 +196,10 @@ public class ComputerPlayer implements IPlayer
         //TODO IMPROVE!
 
 
-        java.util.Random generator = new java.util.Random();
         System.out.println("I've been here!");
-        int col = generator.nextInt( board.length );
-        int step = 0;
+        VierGewinnt vg = new VierGewinnt();
+        int  col = MaxOlista1( VectorChoices(EddieMemory(), vg.EddieChoices(board)));
+
         while ( isColFull( col, board ) ) {
             col = ( col + 1 ) % board.length;
         }
@@ -173,28 +209,33 @@ public class ComputerPlayer implements IPlayer
 
 
 
+
+
     /**
      * @return true if the column col is already full and false otherwise.
      */
-    private boolean isColFull( int col, Token[][] board )
-    {
-        int topRow = board[ col ].length - 1;
-        return ( board[ col ][ topRow ] != Token.empty );
+    private boolean isColFull(int col, Token[][] board) {
+        int topRow = board[col].length - 1;
+        return (board[col][topRow] != Token.empty);
     }
 
 
-    public void setToken( Token token )
-    {
+    public void DisplayEddieChoices(List<int []> Choices){
+        for(int index = 0; index < Choices.size(); index++)
+            System.out.println("col : " + Choices.get(index)[1] + "  row  :" + Choices.get(index)[0]);
+    }
+
+
+
+    public void setToken(Token token) {
         this.token = token;
     }
 
-    public Token getToken()
-    {
+    public Token getToken() {
         return this.token;
     }
 
-    public String getPlayersName()
-    {
+    public String getPlayersName() {
         return "Random Player";
     }
 }
