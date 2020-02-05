c94974bfb408005467d2e051685db4477cd68a4e
florin-postelnicu <30669906+florin-postelnicu@users.noreply.github.com>
2020-02-05 01:46:46 -0700

    Update EmployeeProject.java

--- IntroArrays/EmployeeProject.java
+++ IntroArrays/EmployeeProject.java
@@ -15,10 +15,10 @@ public class EmployeeProject {
         public String email;
         String ephone;
         public static ArrayList<Employees> listemp = new ArrayList<>();
+        
+        public static HashMap<String, Employees> dictionary = new HashMap<>();
+        
         public Employees(){
-
-
-
         }
 
         public Employees(String name, String user, String password, String phone){
@@ -27,7 +27,7 @@ public class EmployeeProject {
             this.epassword = password;
             this.ephone = phone;
             listemp.add(this);
-            diction.put(this.ename, this );
+            dictionary.put(this.ename, this );
         }
         public void setList() throws Exception {
             for( Employees emps : Listofemployeesfile()){
@@ -36,15 +36,10 @@ public class EmployeeProject {
                 this.epassword = emps.getEpassword();
                 this.ephone = emps.getEphone();
                 listemp.add(this) ;
-                diction.put(this.ename, this)  ;
-
+                dictionary.put(this.ename, this)  ;
             }
         }
-
-        public static HashMap<String, Employees> diction = new HashMap<>();
-
-
-
+        
         public String getEname() {
             return ename;
         }
@@ -88,22 +83,19 @@ public class EmployeeProject {
 
 
         public static ArrayList<Employees>Listofemployeesfile() throws Exception {
+            
             FileReader fileReader = new FileReader("C://Users//postelnicu//IdeaProjects//MoreVectorsLists//src//ListEmployees.txt");
             Scanner scanner = new Scanner(new BufferedReader(fileReader));
-
-
             while (scanner.hasNextLine()) {
-
                 String[] line = scanner.nextLine().split("   ");
-
                 Employees personnel = new Employees(line[0], line[1], line[2], line[3]);
                 Employees.listemp.add(personnel);
-                Employees.diction.put(line[0], personnel);
-
+                Employees.dictionary.put(line[0], personnel);
             }
             return listemp;
         }
         public static void FileWriteEmps(Employees employee) throws IOException {
+            
             BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(new File("C://Users//postelnicu//IdeaProjects//MoreVectorsLists//src//ListEmployees.txt"),true));
             bufferedWriter.newLine();
             StringBuilder stringBuilder = new StringBuilder();
@@ -115,13 +107,9 @@ public class EmployeeProject {
             stringBuilder.append("   ");
             stringBuilder.append(employee.ephone);
             bufferedWriter.write(stringBuilder.toString());
-
             bufferedWriter.close();
 
         }
-
-
-
         public static void main(String[] args) throws Exception {
             listemp = Employees.Listofemployeesfile()  ;
             Scanner scan = new Scanner(System.in);
@@ -134,10 +122,10 @@ public class EmployeeProject {
                 }
             }
             if( spy){
-                System.out.println("The Employee :" + unknown + "  has the  Password: " + diction.get(unknown).getEpassword());
-                System.out.println("The Employee :" + unknown + "  has the  Username:   " + diction.get(unknown).getEuser());
-                System.out.println("The Employee :" + unknown + "  has the  Phone number:  " + diction.get(unknown).getEphone());
-                System.out.println("The Employee :" + unknown + "  has the  Email:   " + diction.get(unknown).getEmail());
+                System.out.println("The Employee :" + unknown + "  has the  Password: " + dictionary.get(unknown).getEpassword());
+                System.out.println("The Employee :" + unknown + "  has the  Username:   " + dictionary.get(unknown).getEuser());
+                System.out.println("The Employee :" + unknown + "  has the  Phone number:  " + dictionary.get(unknown).getEphone());
+                System.out.println("The Employee :" + unknown + "  has the  Email:   " + dictionary.get(unknown).getEmail());
             }
             else{
                 System.out.println("This is not our employee!");
@@ -148,22 +136,20 @@ public class EmployeeProject {
                     empy.setEname(unknown);
                     Scanner scan1 = new Scanner(System.in)  ;
                     System.out.println("Enter user name for " + unknown);
-                    String useremp = scan1.nextLine();
-                    empy.setEuser(useremp);
+                    String userempy = scan1.nextLine();
+                    empy.setEuser(userempy);
 
                     System.out.println("Enter Password for " + unknown);
-                    String pass = scan1.nextLine();
-                    empy.setEpassword(pass);
+                    String passy = scan1.nextLine();
+                    empy.setEpassword(passy);
                     System.out.println("Enter Phone Number  for " + unknown);
-                    String phon = scan1.nextLine();
-                    empy.setEphone(phon);
+                    String phony = scan1.nextLine();
+                    empy.setEphone(phony);
 
                     listemp.add(empy) ;
-                    diction.put(unknown, empy)  ;
-//                     empy.setEname(unknown);
+                    dictionary.put(unknown, empy)  ;
                     System.out.println("The email for  "+ unknown +  "is  "+ empy.getEmail());
                     Employees.FileWriteEmps(empy);
-
                 }
                 else{
                     System.out.println("Ok! I will see you next time! \n\t Have a nice day!");
